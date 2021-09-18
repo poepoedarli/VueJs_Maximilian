e8f3e90960b9c1c280c5d6cf74996a83f0ef64ba
poedarli <poepoe.dev.1991@gmail.com>
2021-09-19 01:01:02 +0800

    Basic data bindings and understanding this keyword

--- app.js
+++ app.js
@@ -3,15 +3,24 @@ const app = Vue.createApp({
         return {//must return
             //return key value paris
             'courseName': 'Vue - The complete guide',
-            'websiteLink': 'https://vuejs.org'
+            'websiteLink': 'https://vuejs.org',
+            'courseGoalA' : '<h1>Learn Vue</h1>',
+            'courseGoalB' : '<h1>Master Vue</h1>'
         }
     },
     methods: {//it also reserved name, like as "data", 
         //inside methods, can set methods as you like
-        rand() {
+        /**
+         * Understanding "this" keyword
+         * Inside data object, we return data method, and it merges into
+         * gloabl Vue object instance, managed by Vue.
+         * Methods also. 
+         * And they do have access to anything stroed into global object through this keyworkd
+         */
+        courseGoal() {
             let value = Math.random();
-            if(value<0.5) return 'Learn Vue';
-            else return 'Master Vue';
+            if(value<0.5) return this.courseGoalA;
+            else return this.courseGoalB;
         }
     },
 });

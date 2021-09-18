1f5e9a8309c2fee0a16fe0ca6f34b6655d8db71e
poedarli <poepoe.dev.1991@gmail.com>
2021-09-19 00:37:32 +0800

    Adding methods in Vue app

--- app.js
+++ app.js
@@ -5,6 +5,14 @@ const app = Vue.createApp({
             'courseName': 'Vue - The complete guide',
             'websiteLink': 'https://vuejs.org'
         }
-    }
+    },
+    methods: {//it also reserved name, like as "data", 
+        //inside methods, can set methods as you like
+        rand() {
+            let value = Math.random();
+            if(value<0.5) return 'Learn Vue';
+            else return 'Master Vue';
+        }
+    },
 });
 app.mount('#course-goal')

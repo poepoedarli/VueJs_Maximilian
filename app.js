6844da6685bd2bf61ac8122374dcb043636afa44
poedarli <poepoe.dev.1991@gmail.com>
2021-09-19 00:34:02 +0800

    Basic binding syntax

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const app = Vue.createApp({
     data: function(){//must be property name "data"
         return {//must return
             //return key value paris
-            'courseName': 'Vue - The complete guide'
+            'courseName': 'Vue - The complete guide',
+            'websiteLink': 'https://vuejs.org'
         }
     }
 });
